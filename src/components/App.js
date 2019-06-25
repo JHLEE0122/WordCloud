702b2823e3f287bdf90c10c4774ee3f196058d42
JHLEE0122 <bestgard98@gmail.com>
2019-06-25 16:47:08 +0900

    intialize React Project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h4>Ha</h4>
+            </div>
+        )
+    }
+}
+export default App;
